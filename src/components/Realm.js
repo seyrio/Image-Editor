5f1107cc599fdfed0978d815b5bfc3c3676262c2
seyrio <agrawal3h2@gmail.com>
2024-01-11 03:26:44 +0530

    image uploader

--- src/components/Realm.js
+++ src/components/Realm.js
@@ -1,4 +1,80 @@
-const Realm = () => {
-  return <div>Welcome to the Realm!</div>;
-};
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { useNavigate } from "react-router-dom";
+import ImageUploading from "react-images-uploading";
+
+function Realm() {
+  const history = useNavigate();
+  const logOut = () => history("/");
+
+  const [images, setImages] = useState([]);
+  const maxNumber = 1;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="/realm">Image Editor Demon</Navbar.Brand>
+          <Button variant="outline-success" onClick={logOut}>
+            LogOut
+          </Button>
+        </Container>
+      </Navbar>
+
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          //   onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: "red" } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            {/* <button onClick={onImageRemoveAll}>Remove all images</button> */}
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img
+                  src={image["data_url"]}
+                  alt=""
+                  style={{ "max-height": "70vh" }}
+                  className="mw-70"
+                />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
+    </div>
+  );
+}
+
 export default Realm;
